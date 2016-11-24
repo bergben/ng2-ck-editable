45c8058aa624cb19016815263200efa8402c133d
Benedikt Berger <benedikt.berger@live.de>
2016-11-24 11:41:37 +0100

    commonModule bugfix

--- src/ng2-ck-editable.module.ts
+++ src/ng2-ck-editable.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ModuleWithProviders, OpaqueToken } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Ng2CKEditableComponent } from './ng2-ck-editable.component';
 import { CKEditableOptions, CKEditableOptionsInterface } from './ng2-ck-editable.interface';
 import { CKEditorModule, CKEditorComponent } from 'ng2-ckeditor';
@@ -12,24 +13,25 @@ export function optionsFactory(userOptions: CKEditableOptions): CKEditableOption
 }
 
 @NgModule({
-    imports: [
-        CKEditorModule,
-    ],
-    entryComponents: [
-        CKEditorComponent
-    ],
-    declarations: [Ng2CKEditableComponent],
-    exports: [Ng2CKEditableComponent]
+  imports: [
+    CKEditorModule,
+    CommonModule
+  ],
+  entryComponents: [
+    CKEditorComponent
+  ],
+  declarations: [Ng2CKEditableComponent],
+  exports: [Ng2CKEditableComponent]
 })
 export class Ng2CKEditableModule {
-    static forRoot(options: CKEditableOptionsInterface = {}): ModuleWithProviders {
+  static forRoot(options: CKEditableOptionsInterface = {}): ModuleWithProviders {
 
     return {
       ngModule: Ng2CKEditableModule,
-        providers: [{
-          provide: USER_OPTIONS,
-          useValue: options
-        }, {
+      providers: [{
+        provide: USER_OPTIONS,
+        useValue: options
+      }, {
         provide: CKEditableOptions,
         useFactory: optionsFactory,
         deps: [USER_OPTIONS]
